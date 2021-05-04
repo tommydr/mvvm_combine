b4fbca728fecaee36dad7b922604abc0ee5d43d7
Tommy den Reijer <t.ommy@hotmail.nl>
2021-05-04 19:09:08 +0200

    Initial Commit

---
+++ mvvm_combine/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  mvvm_combine
+//
+//  Created by Tommy den Reijer on 04/05/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ mvvm_combine/mvvm_combineApp.swift
@@ -0,0 +1,17 @@
+//
+//  mvvm_combineApp.swift
+//  mvvm_combine
+//
+//  Created by Tommy den Reijer on 04/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct mvvm_combineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
